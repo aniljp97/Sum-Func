1de27e0430b54cd529f1daa959b75eeee0058b4f
aniljp97 <aniljp97@users.noreply.github.com>
2022-08-12 22:15:03 -0500

    Add files via upload

---
+++ Sum Func/Sum Func/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumFunc
+{
+    class Program
+    {
+        static long Sum(int[] arr)
+        {
+            long sum = 0;
+            foreach(int e in arr)
+            {
+                sum += e;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(new int[] { 1,2,3}) == 6);
+            Console.WriteLine(Sum(new int[] { 6, 2, 2 }) == 10);
+            Console.WriteLine(Sum(new int[] { 11, -2, -10 }) == -1);
+            Console.WriteLine(Sum(new int[] { 1, -1, 0 }) == 0);
+        }
+    }
+}
